ef5d8b43a8a1aae4ed7cc85550cf5ba399bde02e
Gallahard <munarok22@gmail.com>
2026-06-20 13:01:37 +0700

    print prompt (belum tuntas)

--- input/input.asm
+++ input/input.asm
@@ -1,11 +1,15 @@
 SECTION .data
-    PROMPT          DB  "Enter your name: ", 0        ; Prompt Text
+    PROMPT          DB  "Enter your name: ", 0        ; Prompt  Text
     DISPLAY_NAME    DB  "Your name is: ",    0        ; Display Text
 
 SECTION .bss
-    NAME RESB 100    ; Sedia wadah sepanjang 100 byte
+    FIRST_NAME    RESB    50    ; Sedia wadah nama depan    sepanjang 50 byte
+    LAST_NAME     RESB    50    ; Sedia wadah nama belakang sepanjang 50 byte
 
 SECTION .text
     global _start
 
-_start:
+    _start:                             ; !!! INSTRUKSI DIMULAI DISINI !!!
+        ; [ PRINT PROMPT ]
+        MOV ESI, PROMPT                 ; Pindahkan alamat PROMPT ke wadah ESI (Wadah source/sumber)
+        MOV EDX, DISPLAY_NAME - PROMPT  ; Hasil perhitungan panjang text dipindahkan ke wadah EDX (Wadah data)
